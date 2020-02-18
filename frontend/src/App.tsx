937069656e8243c89dbd7e7845e4d3319b60afaf
Paulo <pauloeduardo2709@gmail.com>
2020-02-18 01:47:36 -0300

    💄 Add dynamic background and author credits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,34 @@ import "./styles/App.scss";
 import TodoBox from "./components/TodoBox";
 import TrelloIntegration from "./components/TrelloIntegration";
 import CalendarIntegration from "./components/CalendarIntegration";
+import backgroundInfo from "./assets/background.json";
 
-class App extends Component {
+interface AppState {
+  backgroundAuthor: string;
+  backgroundAuthorLink: string;
+}
+
+class App extends Component<any, AppState> {
+  state = { backgroundAuthor: "", backgroundAuthorLink: "" };
+
+  componentDidMount() {
+    let background: any;
+
+    const now = new Date();
+    if (now.getHours() >= 6 && now.getHours() <= 18) {
+      background = backgroundInfo.day;
+    } else {
+      background = backgroundInfo.night;
+    }
+
+    document.getElementsByTagName("body")[0].background = background.image;
+
+    this.setState({
+      backgroundAuthor: background.author.name,
+      backgroundAuthorLink: background.author.link
+    });
+  }
+
   handleTrelloSave = (apiKey?: string, listId?: string) => {
     localStorage.setItem("trello-config", JSON.stringify({ apiKey, listId }));
   };
@@ -44,6 +70,17 @@ class App extends Component {
           <TodoBox />
           <CalendarIntegration />
           {this.loadTrelloIntegration()}
+        </div>
+        <div id="background-info">
+          Image by{" "}
+          <a
+            href={this.state.backgroundAuthorLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.backgroundAuthor}
+          </a>
+          .
         </div>
       </React.Fragment>
     );
