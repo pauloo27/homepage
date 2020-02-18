4d55d702900017e61c565380d5245031e3958fdc
Paulo <pauloeduardo2709@gmail.com>
2020-02-18 15:08:14 -0300

    Updates

---
+++ precache-manifest.77d4fd836b940389c31ff80666ae06fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a8c3c4d82593830a69c851f0b439df",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "530e743642fef7e78a2f",
+    "url": "/homepage/static/css/2.4aadc2c1.chunk.css"
+  },
+  {
+    "revision": "6ce2cdb1e6cf53aca23e",
+    "url": "/homepage/static/css/main.c8af95b1.chunk.css"
+  },
+  {
+    "revision": "530e743642fef7e78a2f",
+    "url": "/homepage/static/js/2.4b1648a1.chunk.js"
+  },
+  {
+    "revision": "5776768c082169f6074c75f93f8b9356",
+    "url": "/homepage/static/js/2.4b1648a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce2cdb1e6cf53aca23e",
+    "url": "/homepage/static/js/main.9d8788d9.chunk.js"
+  },
+  {
+    "revision": "97bc4cd87188df5b13ec",
+    "url": "/homepage/static/js/runtime-main.06482b8a.js"
+  }
+]);
