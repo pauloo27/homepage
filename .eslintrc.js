8f249cdf95d77ea3bb6fa69ce29d5be474691931
Paulo <pauloeduardo2709@gmail.com>
2020-05-14 22:18:48 -0300

    🔧 Change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
   rules: {
     "react/prefer-stateless-function": "off",
     "jsx-a11y/no-static-element-interactions": "off",
+    "class-methods-use-this": "off",
     "import/extensions": "off",
     "jsx-a11y/click-events-have-key-events": "off",
     "react/state-in-constructor": "off",
