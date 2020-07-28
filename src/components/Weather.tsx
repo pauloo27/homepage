c566523d9b02ae85f436edf2154f58062403ac41
Paulo <pauloeduardo2709@gmail.com>
2020-07-28 18:08:34 -0300

    ⚡️ Add cache to weather

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -3,10 +3,26 @@ import React, { useState, useEffect } from "react";
 export default function Weather() {
   const [weather, setWeather] = useState(undefined as string | undefined);
   useEffect(() => {
+    const weatherCache = localStorage.getItem("weather-cache");
+    const now = new Date().getTime() / 1000;
+    
+    if(weatherCache !== null) {
+      console.log("Weather cache found...");
+      const {date, data} = JSON.parse(weatherCache);
+      if(now < date + (60 * 30)) {
+        setWeather(data);
+        return;
+      }
+      console.log("Weather cache is too old.")
+    }
+
     console.log("Fetching weather...");
     fetch("https://wttr.in/?view=3")
       .then(res => res.text())
-      .then(text => setWeather(text));
+      .then(text => {
+        localStorage.setItem("weather-cache", JSON.stringify({date: now, data: text}));
+        setWeather(text);
+      });
   }, [setWeather])
   
   return (<h6 id="weather">{weather || "Fetching weather"}</h6>);
