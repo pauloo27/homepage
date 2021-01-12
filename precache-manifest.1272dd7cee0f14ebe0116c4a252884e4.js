c3b14179ae506ad96e02708bab9ec8c38c65304a
Pauloo27 <pauloeduardo2709@gmail.com>
2021-01-12 20:01:21 +0000

    Deploying to gh-pages from  @ 30ba5dee8dcae929f9d25195dd9ceb611fcc2fe6 🚀

---
+++ precache-manifest.1272dd7cee0f14ebe0116c4a252884e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8d726f004c0bc34b1190a30ad9d520",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "c8899f4111b5cfec9aed",
+    "url": "/homepage/static/css/2.db1bf4f3.chunk.css"
+  },
+  {
+    "revision": "de52c9a45c797f602e93",
+    "url": "/homepage/static/css/main.ee3a0a3e.chunk.css"
+  },
+  {
+    "revision": "c8899f4111b5cfec9aed",
+    "url": "/homepage/static/js/2.954b1721.chunk.js"
+  },
+  {
+    "revision": "5f3c591ec8417f8049bc645446a5c8ab",
+    "url": "/homepage/static/js/2.954b1721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de52c9a45c797f602e93",
+    "url": "/homepage/static/js/main.7a552467.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  }
+]);
